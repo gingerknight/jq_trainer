c6efcb6d2d1e32c556ecf6b238dbf8c64db4d9de
gingerknight <ryanrichmond6@gmail.com>
2025-05-26 23:22:26 -0600

    Add: gh actions and tmuxclass

--- tmux_session.py
+++ tmux_session.py
@@ -4,10 +4,26 @@
 import libtmux
 
 
-server = libtmux.Server()
-session = server.new_session(session_name="jq_trainer", kill_session=True)
-window = session.attached_window
-pane1 = window.panes[0]
-pane1.send_keys("echo 'Hello, World!'")
+# Tmux Session Creation Class
+class TmuxSession:
+    def __init__(self, session_name: str = "jq_trainer"):
+        """
+        Initialize a TmuxSession instance.
+        :param session_name: Name of the tmux session to create or attach to.
+        """
+        self.session_name = session_name
+        self.server = libtmux.Server()
+        self.session = self.get_or_create_session()
+
+    def get_or_create_session(self):
+        """
+        Create a new tmux session, kills any existing session with the same name.
+        """
+        return self.server.new_session(session_name=self.session_name, kill_session=True)
+
+
 # This code creates a new tmux session named "jq_trainer", kills any existing session with that name,
 # and sends the command to echo "Hello, World!" in the first pane of the attached window.
+window = TmuxSession("jq_trainer").session.active_window
+pane1 = window.panes[0]
+pane1.send_keys("echo 'Hello, World!'")
